1c2a2eb2b0a31d8d2b122e0c933113120ff616a0
gulay-yusifli <yusifligulay06@gmail.com>
2026-03-01 08:12:45 +0000

    task

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -9,28 +9,31 @@ const hostname = '127.0.0.1';
 const port = 1245;
 
 const app = http.createServer(async (req, res) => {
-  res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
 
   const { url } = req;
 
   if (url === '/') {
-    res.write('Hello Holberton School!');
-  } else if (url === '/students') {
+    res.statusCode = 200;
+    res.end('Hello Holberton School!');
+    return;
+  }
+
+  if (url === '/students') {
+    res.statusCode = 200;
     res.write('This is the list of our students\n');
     try {
       const students = await countStudents(DATABASE);
-      res.end(`${students.join('\n')}`);
+      res.end(students.join('\n'));
     } catch (error) {
       res.end(error.message);
     }
+    return;
   }
   res.statusCode = 404;
   res.end();
 });
 
-app.listen(port, hostname, () => {
-  //   console.log(`Server running at http://${hostname}:${port}/`);
-});
+app.listen(port, hostname);
 
 module.exports = app;
